a8da4d0bb33a4224b04ea480938f11fa9f9fc5a2
AliWayDev <alinurlibekov@gmail.com>
2023-06-08 23:32:43 +0500

    changes

---
+++ src/presentation/order/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Order = () => {
+  return (
+    <>
+      <img
+        src="https://assets-prd.ignimgs.com/2022/10/03/smilee-1664759546368.png"
+        alt=""
+      />
+      <div className="text-center text-2xl font-medium pt-5">Yahu your order goind to you!</div>
+    </>
+  );
+};
